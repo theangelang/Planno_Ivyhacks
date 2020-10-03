898e20f6becc7835c9b1e8d130445cd1817f136f
mohannashahrad <mohannashahrad2001@gmail.com>
2020-10-03 01:17:58 -0400

    some more functions

--- app.py
+++ app.py
@@ -27,11 +27,38 @@ def index():
             db.session.commit()
             return redirect('/')
         except:
-            return 'There was an issue adding your task'
+            return 'OOPS!! THERE IS AN UNEXPECTED ERROR'  
 
     else:
          tasks = ToDo.query.order_by(ToDo.date_created).all()  
          return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = ToDo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:    
+        return 'OOPS!! THERE IS AN UNEXPECTED ERROR'    
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'OOPS!! THERE IS AN UNEXPECTED ERROR'
+
+    else:
+        return render_template('update.html', task=task)        
 
 if __name__ == "__main__" :
     app.run(debug=True) 
