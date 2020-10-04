858374763c45b4c762931797c79e0ee44666c8ef
theangelang <72318135+theangelang@users.noreply.github.com>
2020-10-04 11:09:43 -0400

    Add files via upload

---
+++ GoogleCalendar.py
@@ -0,0 +1,40 @@
+"""
+Note this is a work in progress and much of this code was based from https://github.com/helenxuyang/linked/tree/master/cali_server
+"""
+import datetime
+import json
+import logging
+import os
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+
+def get_calendar_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    # Intended only for debugging / fixing loss of credentials, should 
+    #   hopefully never need to do this
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds, cache_discovery=False)
+
+get_calendar_service()
